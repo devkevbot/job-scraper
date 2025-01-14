4372e0a87cbdf27b258ced9342c034a161c006fe
devkevbot <31908183+devkevbot@users.noreply.github.com>
2025-01-14 14:46:50 -0800

    Scrape jobs from https://linusmediagroup.com/careers

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	res, err := http.Get("https://linusmediagroup.com/careers")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var jobs []string
+
+	doc.Find(".accordion-item__title").Each(func(i int, s *goquery.Selection) {
+		jobs = append(jobs, strings.TrimSpace(s.Text()))
+	})
+
+	for _, j := range jobs {
+		fmt.Printf("%s\n", j)
+	}
 }
